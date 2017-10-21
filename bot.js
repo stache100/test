b8349608198ffe7c4daf84e6d857f32bd46895f4
stache100 <dustinpham25@yahoo.com>
2017-10-21 00:42:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,49 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+const settings =require('./settings.json');
+const prefix = settings.prefix;
+const fs = require('fs');
+const Enmap = require("enmap");
+const EnmapLevel = require("enmap-level");
 
-client.on('ready', () => {
-	console.log('I am ready!');
+process.on("unhandledRejection", err => {
+  console.error("Uncaught Promise Error: ", err);
 });
 
-client.on('message', message => {
-	if (message.content === 'ping') {
-		message.reply('pong');
-	}
+var config={
+	prefix : settings.prefix,
+	botInstance : false
+};
+
+var commands = new Enmap();
+var aliases = new Enmap();
+
+fs.readdir(`./awards/`, (err, files) => { // Load commands from files
+  if (err) console.error(err);
+  console.log(`# of award commands: ${files.length}`);
+  files.map(f => {
+    let props = require(`./awards/${f}`);
+    commands.set(props.help.name, props);
+  });
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.on('message', msg => {
+bot.user.setGame('>help')
+
+var input = msg.content.toLowerCase();
+
+	if (msg.author === bot.user) return;
+  if (!msg.content.startsWith(prefix)) return;
+
+  let command = msg.content.split(' ')[0].slice(prefix.length).toLowerCase();
+  let args = msg.content.split(" ").slice(1);
+
+  if (command != "") console.log(">" + command);
+
+  if (commands.has(command)) {
+    let cmd = commands.get(command);
+    cmd.run(config, msg, args);
+  }
+
+});
+bot.login(process.env.BOT_TOKEN);
